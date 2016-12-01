c57c2a1fab3d8cc623cc5e0debe5eb0ed643b1e9
Barsha Maharjan <barsha@parakhi.com>
2016-12-01 18:08:38 +0545

    layout changes

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 
+	//=career page modal ficed top
+	var controller = new ScrollMagic.Controller();
+
+	var pintopmenu = new ScrollMagic.Scene({
+		triggerElement: '#modalspace1',
+		triggerHook : 0,
+		duration: '300%'
+	})
+	.setPin('#modalspace1')
+	.addTo(controller);
+
+	var pintopmenu2 = new ScrollMagic.Scene({
+		triggerElement: '#modalspace2',
+		triggerHook : 0,
+		duration: '300%'
+	})
+	.setPin('#modalspace2')
+	.addTo(controller);
+
     //loader
 
     var tl = new TimelineLite({paused: true}),
@@ -162,22 +181,18 @@ $(document).ready(function(){
 	.addTo(controller);
 
 
-
 	//pen effect
 
-		// move the pen body up to connect with the first part
+	// move the box up to connect with the first part
 	TweenMax.set('.part3', {y: -750});
 
-	// hide all headings and text
-	//TweenMax.set(['.parts h2, .parts p'], {autoAlpha: 0});
-
-	// create a tween that will move the pen body back to its original css position
+	// create a tween that will move the box back to its original css position
 	var bodyToStart = TweenMax.to('.part3', 1, {y: 0, ease:Linear.easeNone});
 
 	// Init ScrollMagic Controller
 	var controller = new ScrollMagic.Controller();
 
-	// Create a Scene 1 - move pen body back to start - HOW?
+	// Create a Scene 1 - move box back to start
 	var bodyToStartScene = new ScrollMagic.Scene({
 		triggerElement: '.part1',
 		triggerHook: 1,
@@ -187,40 +202,7 @@ $(document).ready(function(){
 	.setTween(bodyToStart)
 	.addTo(controller);
 
-	// move 4 and 5 down, to connect with part 6 - HOW?
-	// TweenMax.set('.part5', {y: 120});
-	// TweenMax.set('.part4', {y: 289});
-
-	// move part 6 to connect with the body
-	// TweenMax.set('.part6', {y: -846});
-
-	// move 4 and 5 to connect with part 6 again
-	// TweenMax.set('.part5', {y: -726}); /* 120 - 846 */
 	TweenMax.set('.part4', {y: -557}); /* 289 - 846 */
-
-	// Create scene for part 6 - move part 6 back to start
-	// var p6ToStart = new TweenMax.to('.part6', 1, {y:0, ease:Linear.easeNone});
-
-	// var p6ToStartScene = new ScrollMagic.Scene({
-	// 	triggerElement: '.part1',
-	// 	triggerHook: 1,
-	// 	duration: 846,
-	// 	offset: 1250
-	// })
-	// .setTween(p6ToStart)
-	// .addTo(controller);
-
-	// Create scene for part 5 - move part 5 back to start
-	// var p5ToStart = new TweenMax.to('.part5', 1, {y:0, ease:Linear.easeNone});
-
-	// var p5ToStartScene = new ScrollMagic.Scene({
-	// 	triggerElement: '.part1',
-	// 	triggerHook: 1,
-	// 	duration: 726,
-	// 	offset: 1250
-	// })
-	// .setTween(p5ToStart)
-	// .addTo(controller);
 
 	// Create scene for part 4 - move part 4 back to start
 	var p4ToStart = new TweenMax.to('.part4', 1, {y:0, ease:Linear.easeNone});
